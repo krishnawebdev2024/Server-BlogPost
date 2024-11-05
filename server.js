eec48088806ad63cef6e4db733c8195678811ab0
krishna <krisch.webdev@gmail.com>
2024-11-05 17:20:04 +0100

    changedtheexistingport

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-import express, {json} from 'express';
-import cors from 'cors';
+import express, { json } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
-import blogPostsRoutes from './routes/blogPostsRoutes.js';
+import blogPostsRoutes from "./routes/blogPostsRoutes.js";
 
-import {createBlogSchema} from './database/createBlogSchema.js';
-
+import { createBlogSchema } from "./database/createBlogSchema.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());  // Enable CORS for all routes
+app.use(cors()); // Enable CORS for all routes
 app.use(json());
 
 app.use("/api/v1/BlogPosts", blogPostsRoutes);
 // Connect to the database
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get("*", (req, res) => { 
-    res.status(404).json({ error: "Not found" });
-  });
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    createBlogSchema();  // Create BlogPosts table if it doesn't exist
-    
-})
+  console.log(`Server running on port ${port}`);
+  createBlogSchema(); // Create BlogPosts table if it doesn't exist
+});
